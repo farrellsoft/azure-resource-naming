390d1665da33335a64e460ea6580f9e30b76a3d8
Jason Farrell <imjason@gmail.com>
2023-02-20 22:35:30 -0500

    fixed syntax error

--- global.tf
+++ global.tf
@@ -5,6 +5,6 @@ locals {
   environment_lower           = lower(var.environment)
   environment_clean           = replace(replace(replace(local.environment_lower, "_", ""), "-", ""), " ", "")
 
-  resource_type_lower         = can(var.resource_type) ? lower(var.resource_type) : ""
+  resource_type_lower         = var.resource_type != null ? lower(var.resource_type) : ""
   resource_type_clean         = replace(replace(replace(local.resource_type_lower, "_", ""), "-", ""), " ", "")
 }
